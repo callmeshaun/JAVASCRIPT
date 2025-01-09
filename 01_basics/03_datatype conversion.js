d88c21a404506341eebdfc712b9db3cfd104fecb
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2025-01-09 14:54:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatype conversion.js
@@ -0,0 +1,25 @@
+let a = "123"
+console.log(typeof a)
+
+let b = Number(a)
+console.log(typeof b)
+console.log(b)
+
+//"33" =>33
+//"33abc" => Nan
+//"shaunak" => Nan
+// false => 0 true => 1
+
+let logged = "shaunak"
+let c = Boolean ( logged)
+console.log(c)
+
+// 1 => true 0=> false
+//"shaunak"=> true
+//""=> false
+
+let d = 33
+let e = String (d)
+
+console.log(e)
+console.log(typeof e)

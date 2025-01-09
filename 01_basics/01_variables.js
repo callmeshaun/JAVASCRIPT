9a5780cad09fcf1eca2113bd06ea5ea6a2aef458
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2025-01-09 14:34:42 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const name="shaunak"
+let email="sshaunak28@gmail.com"
+let age = 28
+
+console.table([id,name,email,age])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+//use strict
+
+//alert(3+3) we are using node js not browser
+
+/* types of data
+This are all primitive data types
+1-number
+2-string
+3-boolean
+4-bigint
+5-null => standalone value
+6-undefined => not defined
+7-symbol => unique
+*/
+
+let age = 18
+let name = "shaunak"
+
+console.log(typeof(age))
+
+console.log(typeof(undefined))
+//undefined is a type
+
+console.log(typeof null)
+// null type is object

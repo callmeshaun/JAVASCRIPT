cae60a336da4a787fa0c3522921ea510105e4326
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2025-01-09 15:14:15 +0000

    datatype conversion

--- 01_basics/03_datatype conversion.js
+++ 01_basics/03_datatype conversion.js
@@ -1,25 +1,49 @@
-let a = "123"
-console.log(typeof a)
+// let a = "123"
+// console.log(typeof a)
 
-let b = Number(a)
-console.log(typeof b)
-console.log(b)
+// let b = Number(a)
+// console.log(typeof b)
+// console.log(b)
 
 //"33" =>33
 //"33abc" => Nan
 //"shaunak" => Nan
 // false => 0 true => 1
 
-let logged = "shaunak"
-let c = Boolean ( logged)
-console.log(c)
+// let logged = "shaunak"
+// let c = Boolean ( logged)
+// console.log(c)
 
 // 1 => true 0=> false
 //"shaunak"=> true
 //""=> false
 
-let d = 33
-let e = String (d)
+// let d = 33
+// let e = String (d)
 
-console.log(e)
-console.log(typeof e)
+// console.log(e)
+// console.log(typeof e)
+
+/******* operation ******* */
+
+// let a = 3
+// let negValue= -a
+// console.log(negValue) 
+
+// console.log(2+2) 
+// console.log(2*2) 
+// console.log(2-2) 
+
+// let a = "shaunak"
+// let b = "suryawanshi"
+// console.log(a+b)
+// console.log("1"+2)
+
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(+true)
+/*not prefered this type of code */
+
+
+

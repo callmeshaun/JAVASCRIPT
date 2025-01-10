fd8cb76cb2bcb05e817010fc97da42504fc7b66e
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2025-01-10 14:41:34 +0000

    summary

---
+++ 01_basics/datatype summary.js
@@ -0,0 +1,47 @@
+// types of data types
+/* primitive datatypes
+
+int => int
+string=> string
+boolean=>boolean
+null=>object
+undefined=>
+symbol=>
+bigint=> bigint
+
+Non primitive
+
+array
+object 
+function
+*/
+
+// js is a dynamic typed languaged Because data type will automatically assigned at the time of compilation or code execution
+
+//array
+
+let a=["iron man","hulk"]
+console.log(typeof a)
+//=>object
+
+let myArray=Array["shaunak",234]
+console.log(typeof myArray)
+//=>object
+
+//object
+let b ={
+    name:"shaunak",
+    age:21,
+}
+//console.log(typeof b)
+//=>object
+
+//function
+
+const myFunction=function(){
+    console.log("Hello wrld")
+}
+
+//console .log(typeof myFunction)
+//=>function
+

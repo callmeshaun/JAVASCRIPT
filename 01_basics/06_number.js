a12ac53b90bbac4a972deb7ede727bf1b211f035
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2025-01-13 14:59:04 +0000

    numbers

---
+++ 01_basics/06_number.js
@@ -0,0 +1,8 @@
+const numberr = 100
+
+console.log(numberr.toString().length);
+console.log(typeof (numberr));
+
+const numbb=123789
+console.log(numbb.toPrecision(3))
+console.log(numbb.toFixed(1))

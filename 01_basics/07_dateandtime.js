39c58504f07494a41895d00017adbd52b70f3d4c
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2025-02-23 09:07:53 +0000

    datetime

---
+++ 01_basics/07_dateandtime.js
@@ -0,0 +1,38 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+//CREATE DATE
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date("2025-02-23")
+// console.log(myCreatedDate.toLocaleString());
+
+//TIME STAMP
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+//Millisecond
+// console.log(Math.floor(Date.now()/1000))
+
+// let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getDate())
+// console.log(newDate.getDay())
+// console.log(newDate.getMonth())
+
+// `${newDate.getDay()}`
+
+//  newDate.LocateString('default',{
+//     weekday:"long"
+// })
+
+
+
